a3a188d4f5a21e9dc06a0bb54bed7bada7080b73
Tkaroly84 <127589674+Tkaroly84@users.noreply.github.com>
2023-06-17 13:53:39 +0200

    Html és gyakorlati

---
+++ index.js
@@ -0,0 +1,37 @@
+// Első feladat
+
+var tároló = [
+  [1, 2, 3],
+  ['alma', 'banán', 'körte'],
+  { segítő: 'János', kor: 31 },
+  'Károly', 'Ricsi'
+];
+
+function checkName() {
+  var név = document.getElementById('név').value;
+  var válasz = document.getElementById('válasz');
+
+  if (tároló.includes(név)) {
+    válasz.textContent = 'Regisztrált tag vagy.';
+    var segítő = tároló[2].segítő;
+    válasz.textContent += ' A Te segítőd: ' + segítő + '( hozzá fordulhatsz segítségért)';
+  } else {
+    válasz.textContent = 'Nem vagy regisztrált tag.';
+  }
+}
+
+// Második feladat
+
+const számokTömb = [10, 30, 50, 70, 90, 100];
+
+function számolás() {
+  const number = parseInt(document.getElementById('szám').value);
+  const következőSzám = számokTömb.find(num => num > number);
+  const válasz2 = document.getElementById('válasz2');
+
+  if (következőSzám !== undefined) {
+    válasz2.textContent = 'A következő szám: ' + következőSzám;
+  } else {
+    válasz2.textContent = 'Nincs következő szám.';
+  }
+}
